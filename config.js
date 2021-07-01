3def87bd76e0f1c390332bfdf72957455f10e390
Huyhuynh811 <86771029+Huyhuynh811@users.noreply.github.com>
2021-07-01 16:38:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: '"Annie"',
+    introDesc: `Một điều sến rện trong mùa dịch`,
+    btnIntro: '💜',
+    title: 'Ai lò vé Du',
+    desc: 'Du có thương Me khum',
+    btnYes: 'Thưng lúm 💜',
+    btnNo: 'Khum, lìu liu 😂',
+    title: 'Hehe, biết là thương lúm mè',
+    title: 'Dạo này bận quá nên hay bỏ bê em',
+    title: 'Nhưng khum sao vẫn có thời gian rãnh để tặng em cái thứ sến rện này 😉',
+    question: 'Sau dịch muốn đi ăn gì',
+    btnreply: 'Quăng dô đây',
+    mess: 'Hết gòi, sến vậy đủ gòi 😁',
+    messDesc: 'Ráng đợi hết dịch nhé',
+    btnAccept: 'Kê lun ❤',
 }
